0790c006060dab10b548bba92342e2b7ee778711
Adriaan van Schalkwyk <adriaan.vanschalkwyk@absa.africa>
2021-04-24 14:56:03 +0200

    initial setup

--- News App/News App/NewsTableViewController.swift
+++ News App/News App/NewsTableViewController.swift
@@ -1,19 +1,16 @@
 //
-//  ViewController.swift
+//  NewsTableViewController.swift
 //  News App
 //
 //  Created by Adriaan van Schalkwyk on 2021/04/24.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class NewsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
