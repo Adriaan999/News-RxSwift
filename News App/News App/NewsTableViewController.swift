8af1281f1da95ccb8d09760cabf3abc97d931500
Adriaan van Schalkwyk <adriaan.vanschalkwyk@absa.africa>
2021-04-28 16:24:34 +0200

    add RxSwift

--- News App/News App/NewsTableViewController.swift
+++ News App/News App/NewsTableViewController.swift
@@ -7,10 +7,17 @@
 
 import Foundation
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NewsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    private func populateNews() {
+        let key = APIKey.newsApiKey
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=2ce38f256a324f99a24af17fe993082a")
     }
 }
